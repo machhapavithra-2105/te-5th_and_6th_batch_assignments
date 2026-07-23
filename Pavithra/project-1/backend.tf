cb19e866896c81a7c11002cff8be4c7702b848d2
pavithra <machhapavithra@gmail.com>
2026-07-23 12:05:09 +0530

    Remove Terraform project-1

--- Pavithra/project-1/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-bucket1-project1"
-    key          = "terraform-project-1/terraform.tfstate"
-    region       = "ap-south-1"
-    use_lockfile = true
-  }
-}
- 
